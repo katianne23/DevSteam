2fc4ec8624b838b75d69ec3f290a48e47e3a5ddd
Katianne23 <katianne40@gmail.com>
2023-07-12 10:37:10 -0300

    Create salecard

---
+++ src/components/cards/saleCard/SaleCard.jsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+import styles from "./SaleCard.module.css";
+import Button from "@/components/forms/button/Button";
+
+const SaleCard = () => {
+  return (
+      <div className={styles.salecard}>
+        <Image src='/products/league-of-legends.jpg' width={250} height={300}  />
+        <div className={styles.info}>
+            <h3 className={styles.title}>Ofertas Exclusiva</h3>
+            <div className={styles.pricecard}>
+                <div className={styles.percent}>-20%</div>
+                <div className={styles.price}>
+                    <p className={styles.fullprice}>R$99,90</p>
+                    <h4 className={styles.discountprice}>R$89,90</h4>
+                </div>
+            </div>
+            <Button>Adicionar ao carrinho</Button>
+        </div>
+      </div>
+
+  );
+};
+
+export default SaleCard;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Head from 'next/head';
 import Navbar from '@/components/navbar/Navbar';
 import Subtitle from '@/components/tipography/subtitle/Subtitle';
 import Container from '@/components/container/Container';
+import SaleCard from '@/components/cards/saleCard/SaleCard';
 
 export default function Home() {
   return (
@@ -18,6 +19,9 @@ export default function Home() {
         <Container>
           <div>
             <Subtitle >Promoções</Subtitle>
+            <div>
+              <SaleCard />
+            </div>
           </div>
           <div>
             <Subtitle >Outros Jogos</Subtitle>
